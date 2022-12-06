0544abf1d376dc2a194b9b293065b8525aa2f6c1
Pascal Thomet <pthomet@gmail.com>
2022-12-06 18:31:38 +0100

    (Bundle) Disable one Selectable overload
    as this leads to two identical signatures (see https://github.com/pthom/imgui_bundle/issues/36)

--- imgui_widgets.cpp
+++ imgui_widgets.cpp
@@ -7445,6 +7445,7 @@ bool ImGui::Selectable(const char* label, bool selected, ImGuiSelectableFlags fl
     return pressed; //-V1020
 }
 
+#ifndef IMGUI_BUNDLE_BUILD_PYTHON
 bool ImGui::Selectable(const char* label, bool* p_selected, ImGuiSelectableFlags flags, const ImVec2& size_arg)
 {
     if (Selectable(label, *p_selected, flags, size_arg))
@@ -7454,6 +7455,7 @@ bool ImGui::Selectable(const char* label, bool* p_selected, ImGuiSelectableFlags
     }
     return false;
 }
+#endif
 
 
 //-------------------------------------------------------------------------
