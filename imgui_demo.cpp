519156d47aa33a790ff29cc55ad76154ff9d0dcd
Pascal Thomet <pthomet@gmail.com>
2023-12-07 13:45:28 +0100

    [Bundle] add ImGuiDemoSetShowPythonCode

--- imgui_demo.cpp
+++ imgui_demo.cpp
@@ -9227,6 +9227,12 @@ namespace ImGuiDemoMarkerCodeViewer_Impl
             ImGui::End();
         }
 
+        // [Bundle]
+        void SetShowPythonCode(bool showPythonCode)
+        {
+            ShowPythonCode = showPythonCode;
+        }
+
     private:
         void ReadSource(const char* source_file)
         {
@@ -9419,6 +9425,13 @@ namespace ImGuiDemoMarkerCodeViewer
         ImGuiDemoMarkerCodeViewer_Impl::GDemoCodeWindow().NavigateTo(line_number, section);
     }
 }
+
+// [Bundle]
+void ImGuiDemoSetShowPythonCode(bool showPythonCode)
+{
+    ImGuiDemoMarkerCodeViewer_Impl::GDemoCodeWindow().SetShowPythonCode(showPythonCode);
+}
+
 
 // [sub section]  BrowseToUrl()
 // A platform specific utility to open an url in a browser
