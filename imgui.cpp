103796ef5b89645fd5acc0529809693f9e2a2f51
Pascal Thomet <pthomet@gmail.com>
2024-02-06 19:47:19 +0100

    [Bundle] Add GetIniFilename / GetLogfilename

--- imgui.cpp
+++ imgui.cpp
@@ -2063,6 +2063,15 @@ void ImGuiIO::SetLogFilename(const char* filename)
     strncpy(sLogFilename, filename, 1024);
     LogFilename = sLogFilename;
 }
+std::string ImGuiIO::GetIniFilename()
+{
+    return IniFilename;
+}
+std::string ImGuiIO::GetLogFilename()
+{
+    return LogFilename;
+}
+
 #endif
 
 
