216066119500a2f4b46f0271c7b94ab286376500
Pascal Thomet <pthomet@gmail.com>
2024-11-23 20:45:34 +0100

    [Bundle]: glfw Platform_GetClipboardTextFn replace NULL by empty string for python bindings

--- backends/imgui_impl_glfw.cpp
+++ backends/imgui_impl_glfw.cpp
@@ -630,7 +630,19 @@ static bool ImGui_ImplGlfw_Init(GLFWwindow* window, bool install_callbacks, Glfw
 
     ImGuiPlatformIO& platform_io = ImGui::GetPlatformIO();
     platform_io.Platform_SetClipboardTextFn = [](ImGuiContext*, const char* text) { glfwSetClipboardString(NULL, text); };
-    platform_io.Platform_GetClipboardTextFn = [](ImGuiContext*) { return glfwGetClipboardString(NULL); };
+    platform_io.Platform_GetClipboardTextFn = [](ImGuiContext*) {
+        const char* r = glfwGetClipboardString(NULL);
+
+        // This is needed for ImGui Bundle's python bindings, where Platform_GetClipboardTextFn
+        // is intended to return a string, not a char*.
+        #ifdef IMGUI_BUNDLE_PYTHON_API
+        static char empty[] = "";
+        if (r == NULL)
+            r = empty;
+        #endif
+
+        return r;
+    };
 #ifdef __EMSCRIPTEN__
     platform_io.Platform_OpenInShellFn = [](ImGuiContext*, const char* url) { ImGui_ImplGlfw_EmscriptenOpenURL(url); return true; };
 #endif
