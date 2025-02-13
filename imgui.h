11bb0e39d3187e21602acaabc549707469755f32
Pascal Thomet <pthomet@gmail.com>
2025-02-13 22:43:41 +0100

    [Bundle]: Adapt API for Python Binding of ImFont::CalcWordWrapPositionA

--- imgui.h
+++ imgui.h
@@ -3793,7 +3793,15 @@ struct ImFont
     // 'max_width' stops rendering after a certain width (could be turned into a 2d size). FLT_MAX to disable.
     // 'wrap_width' enable automatic word-wrapping across multiple lines to fit into given width. 0.0f to disable.
     IMGUI_API ImVec2            CalcTextSizeA(float size, float max_width, float wrap_width, const char* text_begin, const char* text_end = NULL, const char** remaining = NULL); // utf8
+
+#ifdef IMGUI_BUNDLE_PYTHON_UNSUPPORTED_API
     IMGUI_API const char*       CalcWordWrapPositionA(float scale, const char* text, const char* text_end, float wrap_width);
+#endif
+#ifdef IMGUI_BUNDLE_PYTHON_API
+    // Python API for CalcWordWrapPositionA (will return an index in the text, not a pointer)
+    IMGUI_API int               CalcWordWrapPositionAPython(float scale, const char* text, float wrap_width);
+#endif
+
     IMGUI_API void              RenderChar(ImDrawList* draw_list, float size, const ImVec2& pos, ImU32 col, ImWchar c);
     IMGUI_API void              RenderText(ImDrawList* draw_list, float size, const ImVec2& pos, ImU32 col, const ImVec4& clip_rect, const char* text_begin, const char* text_end, float wrap_width = 0.0f, bool cpu_fine_clip = false);
 
