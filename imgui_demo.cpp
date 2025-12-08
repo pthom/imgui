1b49919003d6bc6a979d25c956cb0710c0417da3
Pascal Thomet <pthomet@gmail.com>
2025-12-08 15:47:20 +0100

    imgui_demo.cpp: IMGUI_DEMO_MARKER can now be defined externally
    IMGUI_DEMO_MARKER can be used to mark sections of the demo and link them to an interactive code browser. In order to use it, define it to an actual macro via force-include.
    
    This commit removes old traces of ImGuiDemoMarkerCallback: its definition is outside the scope of imgui_demo.cpp (and may be added inside the force-included file).
    
    Bottom line, we only need...
    
    ... and nothing more. the rest of the integration depends on the user (e.g. imgui_manual).
    
    This commit also:
    - adds BeginChild / EndChild, so that IMGUI_DEMO_MARKER_SHOW_SHORT_INFO (if present) may stay on top.
    - add one & remove one call to IMGUI_DEMO_MARKER (to maintain a clean hierarchy of sections in the manual)
    
    # Conflicts:
    #	imgui_demo.cpp

--- imgui_demo.cpp
+++ imgui_demo.cpp
@@ -225,6 +225,12 @@ Index of this file:
 #endif
 #endif
 
+// IMGUI_DEMO_MARKER can be used to mark sections of the demo and link them to an interactive code browser.
+// In order to use it, define it to an actual macro via force-include.
+#ifndef IMGUI_DEMO_MARKER
+#define IMGUI_DEMO_MARKER(section)  // Called everywhere in the code to mark interesting sections for the reader.
+#endif
+
 //-----------------------------------------------------------------------------
 // [SECTION] Forward Declarations
 //-----------------------------------------------------------------------------
@@ -282,13 +288,6 @@ static void HelpMarker(const char* desc)
     }
 }
 
-// Helper to wire demo markers located in code to an interactive browser
-typedef void (*ImGuiDemoMarkerCallback)(const char* file, int line, const char* section, void* user_data);
-extern ImGuiDemoMarkerCallback      GImGuiDemoMarkerCallback;
-extern void*                        GImGuiDemoMarkerCallbackUserData;
-ImGuiDemoMarkerCallback             GImGuiDemoMarkerCallback = NULL;
-void*                               GImGuiDemoMarkerCallbackUserData = NULL;
-#define IMGUI_DEMO_MARKER(section)  do { if (GImGuiDemoMarkerCallback != NULL) GImGuiDemoMarkerCallback("imgui_demo.cpp", __LINE__, section, GImGuiDemoMarkerCallbackUserData); } while (0)
 
 // Sneakily forward declare functions which aren't worth putting in public API yet
 namespace ImGui
@@ -437,6 +436,8 @@ void ImGui::ShowDemoWindow(bool* p_open)
 
     ImGui::Text("dear imgui says hello! (%s) (%d)", IMGUI_VERSION, IMGUI_VERSION_NUM);
     ImGui::Spacing();
+
+    ImGui::BeginChild("Demos");
 
     IMGUI_DEMO_MARKER("Help");
     if (ImGui::CollapsingHeader("Help"))
@@ -649,6 +650,7 @@ void ImGui::ShowDemoWindow(bool* p_open)
     DemoWindowInputs();
 
     // End of ShowDemoWindow()
+    ImGui::EndChild(); // </ImGui::BeginChild("Demos");>
     ImGui::PopItemWidth();
     ImGui::End();
 }
@@ -659,7 +661,6 @@ void ImGui::ShowDemoWindow(bool* p_open)
 
 static void DemoWindowMenuBar(ImGuiDemoWindowData* demo_data)
 {
-    IMGUI_DEMO_MARKER("Menu");
     if (ImGui::BeginMenuBar())
     {
         if (ImGui::BeginMenu("Menu"))
@@ -8761,6 +8762,7 @@ static void ShowExampleAppMainMenuBar()
 // (future version will add explicit flags to BeginMenu() to request processing shortcuts)
 static void ShowExampleMenuFile()
 {
+    IMGUI_DEMO_MARKER("Examples");
     IMGUI_DEMO_MARKER("Examples/Menu");
     ImGui::MenuItem("(demo menu)", NULL, false, false);
     if (ImGui::MenuItem("New")) {}
