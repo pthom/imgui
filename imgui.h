0e024c8d5ed8c03af86739a21b768af99aea487c
Pascal Thomet <pthomet@gmail.com>
2026-05-11 17:42:19 +0200

    [Bundle] reviewed add_polyline Python API (for v1.92.8: swapped thickness / flags arg orders)

--- imgui.h
+++ imgui.h
@@ -3720,7 +3720,7 @@ struct ImDrawList
 #ifdef IMGUI_BUNDLE_PYTHON_API
     // - Only simple polygons are supported by filling functions (no self-intersections, no holes).
     // - Concave polygon fill is more expensive than convex one: it has O(N^2) complexity. Provided as a convenience fo user but not used by main library.
-    IMGUI_API void  AddPolyline(const std::vector<ImVec2>& points, ImU32 col, ImDrawFlags flags, float thickness);
+    IMGUI_API void  AddPolyline(const std::vector<ImVec2>& points, ImU32 col, float thickness, ImDrawFlags flags);
     IMGUI_API void  AddConvexPolyFilled(const std::vector<ImVec2>& points, ImU32 col);
     IMGUI_API void  AddConcavePolyFilled(const std::vector<ImVec2>& points, ImU32 col);
 #endif
